6c95961e159a454e1679d76dda9f4545037ed033
Michal-Dudzik <dumichal20@wp.pl>
2022-12-27 01:23:47 +0100

    added place for cities, search bar and icons, just visual tho

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function TopButtons() {
+	const cities = [
+		{
+			id: 1,
+			title: 'London',
+			country: 'UK',
+		},
+		{
+			id: 2,
+			title: 'Paris',
+			country: 'France',
+		},
+		{
+			id: 3,
+			title: 'New York',
+			country: 'USA',
+		},
+		{
+			id: 4,
+			title: 'Tokyo',
+			country: 'Japan',
+		},
+		{
+			id: 5,
+			title: 'Moscow',
+			country: 'Russia',
+		},
+	];
+	return (
+		<div className="flex items-center justify-around my-6">
+			{cities.map((city) => {
+				return (
+					<button key={city.id} className="text-white text-lg font-medium">
+						{city.title}
+					</button>
+				);
+			})}
+		</div>
+	);
+}
+
+export default TopButtons;
